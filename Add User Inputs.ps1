d3508e9d3997edb076bf4b6e4ae05e2de2d23d75
safetibase <49194520+safetibase@users.noreply.github.com>
2019-11-15 17:47:07 +0000

    Updated Install Files
    Updated Install Files

--- Add User Inputs.ps1
+++ Add User Inputs.ps1
@@ -1,8 +1,9 @@
 ﻿########################################
 #
-# SafetIbase Installer - Simple version
+# SafetIbase Installer - Populate Lists with user inputs
 #
 ########################################
+
 
 #---------------------------------------
 # INSTRUCTIONS - READ BEFORE RUNNING SCRIPT
@@ -13,54 +14,79 @@
 # 2) Authorised scripts to be run by PowerShell by changing the 'Execution policy' to remote signed. Refer to https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-6
 # 3) Open as Powershell as administrator
 # 4) Complete the 'User input' section of this script
-# 5) Fill in the excel spreadsheet 'SafetIbaseListsContent - NoUserInput.xlsx' (included in with the install files) for your project.
-# 5) Go to 'Site Contents' > 'Site Assets' and drag and drop the folders contained in the 'Site Asset' folder of the install files.
-# 7) Run the script in one go (f5)
-# 6) once done, your SafetIbase homepage will be available under 'Site Contents' > 'Site Assets' Library > 'pages' > '3.0' > 'dashboard.aspx'
+# 5) Fill in the excel spreadsheet 'SafetIbaseListsContent - UserInput Template.xlsx' (included in with the install files) for your project. Delete any tabs you are not filling in.
+# 5) Run the whole script
 
 #---------------------------------------
-#  Un-comment next line and run this for those who haven't used PowerShell before
+#  Run this for those who haven't used PowerShell before
 #---------------------------------------
 
 #set-executionpolicy remotesigned 
 
 #---------------------------------------
-# INPUTS
+# USER INPUTS
 #---------------------------------------
 
-#---------------------------------------
-# User inputs
-#---------------------------------------
-
-# enter url to SharePoint site where SafetIbase is to be deployed.
-
-$SPsite = "https://teanent.sharepoint.com/teams/pj-b1234"
+$SPsite = "https://teanent.sharepoint.com/teams/pj-b1234" # Change this to match site
 
 #---------------------------------------
-# Fixed inputs
+# FIXED INPUTS
 #---------------------------------------
 
-# If running script by section update the paths to full paths.
-
-$TemplatePath = "$($PSScriptRoot)\List Templates\"
-
-$ExcelListPath = "$($PSScriptRoot)\SafetIbaseListsContent - NoUserInput.xlsx"
+# If running script by section Update the $ExcelLsitPath value to the path to your input spreadseet
+$ExcelListPath = "$($PSScriptRoot)\SafetIbaseListsContent - UserInput Template.xlsx"
 
 
 #---------------------------------------
-# SET UP
+# INSTALL REQUIRED MODULES
 #---------------------------------------
+#Un-comment this section if you have not installed SafetIbase from this computer before
+<#
+function Install-RequiredModules {
+    # Check if required modules are installed. Otherwise install them.
+    Write-Host "`nChecking Dependant Modules...`n"
+
+    $RequiredModules = "ImportExcel","SharePointPnPPowerShellOnline"
+    $output = "ok"
+
+    foreach ($mod in $RequiredModules){
+
+        Try{
+            Get-InstalledModule -Name $mod -ErrorAction Stop | Out-Null
+        } Catch {
+            Write-Host "module $($mod) not installed, installing..."
+            Try{ Install-Module $mod -Force -ErrorAction Stop | Out-Null}
+            Catch {
+                $output = $mod
+            }
+        }
+    }
+
+    $output
+    Write-Host "`nDependant Modules installed`n" -ForegroundColor Green
+
+}
+
+Install-RequiredModules
+#>
 
 #---------------------------------------
-# Import required functions
+# CONNECT TO SHAREPOINT SITE
+#---------------------------------------
+
+Try{
+    $connect = Connect-PnPOnline -Url $SPsite -UseWebLogin -ReturnConnection -ErrorAction Stop
+} Catch{
+    $connect = Connect-PnPOnline -Url $SPsite -ReturnConnection
+}
+
+#---------------------------------------
+# IMPORT REQUIRED FUNCTIONS
 #---------------------------------------
 
 Import-Module "$($PSScriptRoot)\SafetIbaseFunctions.psm1"
 
 <# Used functions
-
-#Install-RequiredModules - Check is ImportExcel and SharePointPnP modules are installed
-
 #Convert object to hashtable
 ConvertTo-HashtableFromPsCustomObject
 
@@ -71,66 +97,6 @@ Get-TopologicalSort
 Get-ClonedObject
 
 #>
-
-#---------------------------------------
-# Install required modules
-#---------------------------------------
-
-Install-RequiredModules
-
-#---------------------------------------
-# Connect to SharePoint site
-#---------------------------------------
-
-Try{
-    $connect = Connect-PnPOnline -Url $SPsite -UseWebLogin -ReturnConnection -ErrorAction Stop
-} Catch{
-    $connect = Connect-PnPOnline -Url $SPsite -ReturnConnection
-}
-
-#---------------------------------------
-# CREATE LISTS (USING THE PROVISIONING TEMPLATE)
-#---------------------------------------
-
-#---------------------------------------
-# Sort lists based on lookup dependencies
-#---------------------------------------
-
-#Get hashtable with all lists and their dependencies
-$Regex = [Regex]::new("(?<={listid:)(.*)(?=})")  #expression to extract cdmSites from {listid:cdmSites}
-$ListsToCreate =  @{}
-
-Get-ChildItem -Path $TemplatePath | ForEach-Object{
-    [XML]$Template = Get-Content -Path $_.PSPath
-    $BaseList = $Template.Provisioning.Templates.ProvisioningTemplate.Lists.ListInstance
-    $DependentList = ($BaseList.Fields.Field | Where-Object {$_.Type -eq 'Lookup'}).List
-    if ($DependentList -eq $null){ $ListsToCreate[$BaseList.Title] = @()}
-    $ListsToCreate[$BaseList.Title] = @($DependentList | ForEach-Object {$Regex.Match($_).Value})
-}
-
-#Created a sorted list of lists to create
-$ListsInOrder = Get-TopologicalSort $ListsToCreate
-$ListOrder = $ListsInOrder | Where-Object {$_} #remove empty strings
-
-#Create all lists from templates
-Foreach ($item in $ListOrder){
-    
-    $TempalteName = "$($TemplatePath)$($item)Template.xml"
-    Write-Host "Creating $($item)..."
-    Apply-PnPProvisioningTemplate -Path $TempalteName -ErrorAction Continue -Verbose
-    Write-Host "Done`n"
-} 
-
-# Add indexes to: cdmHazards list 'Modified' field & cdmStages list 'Title' field
-
-Set-PnPField -List 'cdmHazards' -Identity 'Modified' -Values @{Indexed=$true}
-Set-PnPField -List 'cdmStages' -Identity 'Title' -Values @{Indexed=$true}
-
-#---------------------------------------
-# POPULATE LISTS WITH DEFAULT VALUES FROM EXCEL SPREADSHEET
-#---------------------------------------
-
-# Optional - Can be done in SharePoint
 
 #---------------------------------------
 # Get Lists to populate and sort them
@@ -167,6 +133,8 @@ foreach ($l in $Sheets){
 #Sort list based on dependancies
 $SortedList = Get-TopologicalSort $ListHash
 
+#remove lists not present in Excel sheet
+
 $SortedList2 = @()
 foreach ($ts in $SortedList){
     if($Sheets.Name -contains $ts){
@@ -195,11 +163,11 @@ foreach ($sheet in $SortedList2){
     }
 
     #Import all rows
-    
-              
+
+               
     #Loop through Rows
-    foreach ($row in $ImportedSheet){     
-       
+    foreach ($row in $ImportedSheet){
+               
         #Add list item
         
         $ItemHash = $row | ConvertTo-HashtableFromPsCustomObject
@@ -210,12 +178,9 @@ foreach ($sheet in $SortedList2){
         }
 }
 
-#---------------------------------------
-# Add a link to the SafetIbase dashboard to the rhs navigation menu
-#---------------------------------------
-
-Add-PnPNavigationNode -Location QuickLaunch -Title 'SafetIbase' -Url 'SiteAssets/pages/3.0/dashboard.aspx' -ErrorAction Continue
-
+#>
 #---------------------------------------
 # END OF SCRIPT
 #---------------------------------------
+
+#>
