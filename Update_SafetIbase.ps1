aea4b27bb160f3dd5c005fcd8f4ec65972b243aa
safetibase <49194520+safetibase@users.noreply.github.com>
2020-10-06 19:17:18 +0100

    Add script to update SafetIbase
    Use on a site which already has SAfetIbase installed

---
+++ Update_SafetIbase.ps1
@@ -0,0 +1,57 @@
+﻿#################################
+
+# SafetIbase
+# Update SafetIbase with lates install
+
+#################################
+
+# This will update the relevant site assets, and add indices to the lists as required
+
+
+
+# Change the url to your site then run
+
+$SPsite = "https://teanent.sharepoint.com/teams/pj-b1234"
+
+
+
+$connect = Connect-PnPOnline -Url $SPsite -UseWebLogin -ReturnConnection
+
+# Upload new site assets
+# adapted from https://github.com/MSAdministrator/GetGithubRepository
+
+
+$filesToUpdate = @('SiteAssets/pages/3.0/dashboard.aspx', 'SiteAssets/pages/3.0/scripts/CDM.DATA.CONTROLLER.js', 'SiteAssets/pages/3.0/scripts/DATA.FORMAT.CONTROLLER.js', 'SiteAssets/pages/3.0/scripts/INIT.PAGE.js')
+$TempFolder = 'C:\temp\SafeIbaseUpdate'
+$Owner = 'safetibase'
+$Repository = 'safetibase'
+$Branch = 'master'
+
+foreach ($item in $filesToUpdate){
+
+    Write-Debug -Message "Attempting to create $TempFolder\$item"
+
+    New-Item -ItemType File -Force -Path "$TempFolder\$item" | Out-Null
+
+    $url = "https://raw.githubusercontent.com/$Owner/$Repository/$Branch/$item"
+
+    Write-Debug -Message "Attempting to download from $url"
+
+    ($wc.DownloadString("$url")) | Out-File "$TempFolder\$item"
+
+    Add-PnPFile -Folder $item.Substring(0,$item.LastIndexOf('/')) -Path "$TempFolder\$item" -Verbose
+
+}
+
+
+#Delete temp folder
+Get-ChildItem -Path $TempFolder -recurse | foreach {Remove-Item $_.FullName -Force -Recurse}
+Remove-Item -Path $TempFolder -Force -Recurse
+
+
+# Add indices to: cdmHazards list '& cdmStages list 'Title' field
+
+$fieldsToIndex = 'cdmCurrentStatus','cdmHazardOwner','cdmResidualRiskScore','cdmSite','Created By','Modified','Modified By','cdmStage'
+$fieldsToIndex | set-pnpfield -List 'cdmHazards' -Identity $_ -Values @{Indexed=$true}
+
+Set-PnPField -List 'cdmStages' -Identity 'Title' -Values @{Indexed=$true}
