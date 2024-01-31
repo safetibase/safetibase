bd7fb3e612a50841b7598a80bd63652eabaebcc8
Patrick Hsu <patrick.hsu@mottmac.com>
2024-01-31 09:24:45 +0000

    set up workflow object in config and relevant changes in rvbtn function

--- SiteAssets/pages/3.0/scripts/configData.js
+++ SiteAssets/pages/3.0/scripts/configData.js
@@ -5,7 +5,6 @@ configData = {
     'Client Review': false, // Does the project require a client review to be included in the workflow?
     'Client Name': 'Client', // What is the name of the client of the project?
     'Archive hazards permissions': ['System admin'], // Which user roles should be authorised to archive hazards. This is given as a list. E.g., ['System admin', 'Designer', 'Construction Manager'].
-    'Sync Client Hazard Permissions': ['System admin'], // Defines who can use 'Sync Client Hazards' button.
     'Simplified designer dashboard': false, // Do you want a simplified user dashboard for designers?
     'Simplified design manager dashboard': false, // Do you want a simplified user dashboard for designer managers?
     'Simplified construction engineer dashboard': false, // Do you want a simplified user dashboard for construction engineers?
@@ -20,5 +19,34 @@ configData = {
     'Construction manager approval comment populates cdmSMMitigationSuggestion': false, // Do you want the approval comment from the construction manager's review to populate the site manager's mitigation suggestion
     'Full admin edit rights': false, // Do you want the admin to be able to edit any field, at any point in the workflow?
     'Include contract': true, // Do you want to have the option to assign contracts to hazards?
-    'Exportable workflow states': ['Requires mitigation', 'Assessment in progress', 'Under peer review', 'Under design manager review']
+    'Exportable workflow states': ['Requires mitigation', 'Assessment in progress', 'Under peer review', 'Under design manager review'],
+    
+    //Editable workflow config section. Patrick Hsu, 29 Jan 2024
+    'Workflow' : { 
+        'initiatereview' : { 
+            'nextWorkFlowState': 'Under peer review', 
+            'userRoles': ['Designer','Construction Engineer','Design Manager','Construction Manager','Principal Designer'] // who can review/edit the hazard at this point
+        },
+        'peerreview' : { 
+            'nextWorkFlowState1': 'Under Construction Manager review', 
+            'nextWorkFlowState2': 'Under design manager review', 
+            'userRoles': ['Designer','Construction Engineer'] // who can review/edit the hazard at this point
+        },
+        'dmreview' : { 
+            'nextWorkFlowState': 'Under pre-construction review', 
+            'userRoles': ['Design Manager'] // who can review/edit the hazard at this point
+        },
+        'pcreview' : { 
+            'nextWorkFlowState': 'Under principal designer review', 
+            'userRoles': ['Construction Manager'] // who can review/edit the hazard at this point
+        },
+        'ldreview' : { 
+            'nextWorkFlowState': 'Under site manager review', 
+            'userRoles': ['Principal Designer'] // who can review/edit the hazard at this point
+        },
+        'smreview' : { 
+            'nextWorkFlowState': 'Accepted', 
+            'userRoles': ['Construction Manager'] // who can review/edit the hazard at this point
+        }
+    }
 }
